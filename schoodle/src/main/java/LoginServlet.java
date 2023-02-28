21f363b2589b81c2dfa995dcf3f52567e21a21b7
Aaron Tran <aaron102499@gmail.com>
2023-02-28 04:03:41 -0800

    new login,signup page

--- schoodle/src/main/java/LoginServlet.java
+++ schoodle/src/main/java/LoginServlet.java
@@ -1,12 +1,14 @@
 
 
 import java.io.IOException;
+import java.net.URLEncoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @WebServlet("/login")
@@ -17,7 +19,6 @@ public class LoginServlet extends HttpServlet {
     public void init() throws ServletException {
         try {
             CSVHandler csvHandler = new CSVHandler("src/main/resources/users.csv");
-            System.out.println("test");
             userController = new UserController(csvHandler);
 
         } catch (IOException e) {
@@ -26,15 +27,38 @@ public class LoginServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-
+        throws ServletException, IOException {
+    String action = request.getParameter("action");
+    String username = request.getParameter("username");
+    String password = request.getParameter("password");
+    String fullName = request.getParameter("fullName");
+    
+    if (action.equals("login")) {
         if (userController.login(username, password)) {
-            response.sendRedirect("homepage.html");
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write("{\"success\":true}");
         } else {
-            response.sendRedirect("login.html");
+            response.setContentType("application/json");
+      response.setCharacterEncoding("UTF-8");
+      response.getWriter().write("{\"success\":false,\"message\":\"Invalid username or password\"}");
         }
-        
+    } else if (action.equals("signup")) {
+       
+        if (userController.addUser(username, password, fullName)) {
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write("{\"success\":true");
+            request.getSession().setAttribute("signupSuccess", true);
+            response.sendRedirect("login.html");
+        } else {
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write("{\"success\":false,\"message\":\"Username already exists\"}");
+            
+        }
     }
+}
+
+
 }
