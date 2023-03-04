5d7007e2c30a1f3d33652319822b1e7a7ed11334
Aaron Tran <aaron102499@gmail.com>
2023-03-03 17:52:12 -0800

    Schoodle Project

---
+++ schoodle/src/model/CSVHandler.java
@@ -0,0 +1,46 @@
+package model;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CSVHandler {
+    private String fileName;
+
+    public CSVHandler(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public List<String[]> readAll() throws IOException {
+        List<String[]> data = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                data.add(values);
+            }
+        }
+
+        return data;
+    }
+
+    public void writeAll(List<String[]> data) throws IOException {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            for (String[] values : data) {
+                for (int i = 0; i < values.length; i++) {
+                    writer.append(values[i]);
+
+                    if (i != values.length - 1) {
+                        writer.append(",");
+                    }
+                }
+
+                writer.append("\n");
+            }
+        }
+    }
+}
